64909081355d6408222b3d6d470f158e6ee8e36c
baiwei <1090563464@qq.com>
2020-06-24 16:58:16 +0800

    测è试一下

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -2,6 +2,6 @@ public class main {
     public static void main(String[] args) {
         System.out.println("hello,git");
         System.out.println("老弟啊");
-        System.out.println("你想干啥，我要写个抽奖啊 aaaa ");
+        System.out.println("你想干啥，我要写个抽奖啊bbbaa ");
     }
 }
